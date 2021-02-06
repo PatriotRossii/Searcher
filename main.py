240f3083147515e7804d91cd8245d1f435f3018a
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-06 16:31:35 +0500

    Hotfix

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from PIL import Image
 # Пусть наше приложение предполагает запуск:
 # python search.py Москва, ул. Ак. Королева, 12
 # Тогда запрос к геокодеру формируется следующим образом:
+from utils import get_spn
+
 toponym_to_find = " ".join(sys.argv[1:])
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
