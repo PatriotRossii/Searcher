f367a4875445396f09b2999630a16655d0ac5b94
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-06 16:25:54 +0500

    Move auxillary functions to extern file

--- main.py
+++ main.py
@@ -6,17 +6,6 @@ import requests
 from PIL import Image
 
 
-def get_spn(toponym):
-    envelope = toponym["boundedBy"]["Envelope"]
-    lower_corner = [float(e) for e in envelope["lowerCorner"].split()]
-    upper_corner = [float(e) for e in envelope["upperCorner"].split()]
-
-    delta_x = upper_corner[0] - lower_corner[0]
-    delta_y = upper_corner[1] - lower_corner[1]
-
-    return str(delta_x), str(delta_y)
-
-
 # Пусть наше приложение предполагает запуск:
 # python search.py Москва, ул. Ак. Королева, 12
 # Тогда запрос к геокодеру формируется следующим образом:
